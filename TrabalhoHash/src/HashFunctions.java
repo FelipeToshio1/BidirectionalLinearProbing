7d4dcfeb2f93afa7f6d8a2ee65670a4e61b8f230
Victor Domingos <victordomingos91@gmail.com>
2019-06-18 22:26:47 -0300

    resto

--- TrabalhoHash/src/HashFunctions.java
+++ TrabalhoHash/src/HashFunctions.java
@@ -8,7 +8,6 @@ public class HashFunctions {
     private int increment;
     private int cont;
 
-
     public HashFunctions(int hashmapSize, int increment) {
         this.hashmapSize = hashmapSize;
         this.increment = increment;
@@ -41,7 +40,6 @@ public class HashFunctions {
 
     private ArrayList<Integer> hashmap = new ArrayList<>();
 
-
     public void insert(int key) {
 
         Integer inicialPosition = key % hashmapSize;
@@ -49,49 +47,131 @@ public class HashFunctions {
         if (hashmap.get(inicialPosition) == null) {
             hashmap.set(inicialPosition, key);
         } else {
-            hashmap.set(nextPosition(inicialPosition, increment), key);
 
+            Integer position = nextPosition(inicialPosition, increment);
+            if (position == null) {
+                System.out.println("\nResonance");
+            } else {
+                hashmap.set(position, key);
+            }
         }
     }
 
-    public int nextPosition(int position, int increment) {
+    public Integer nextPosition(int position, int increment) {
 
         int count = 1;
+
         int direction = 2;
 
         while (true) {
 
+            if (count > hashmapSize) {
+                break;
+            }
             int comparator = position + increment * count * (int) Math.pow(-1, direction);
 
-//            if(count > hashmapSize){
-//                System.out.println("Map is Full!");
-//                break;
-//            }
-            if (hashmapSize < comparator ) {
+            if (hashmapSize < comparator) {
                 int aux = hashmapSize - position;
                 comparator = increment - aux;
             }
 
-            if (hashmap.get(comparator) == null) {
-
+            if (hashmapSize > comparator && hashmap.get(comparator) == null) {
                 return comparator;
+
             } else {
 
                 direction++;
                 comparator = position + increment * count * (int) Math.pow(-1, direction);
 
-                if (increment > position){
+                if (increment > position) {
                     int aux = increment - position;
                     comparator = hashmapSize - aux;
                 }
-                if (hashmap.get(comparator) == null) {
-
+                if (hashmapSize > comparator && hashmap.get(comparator) == null) {
 
                     return comparator;
+                }
+
+            }
+            direction++;
+            count++;
+        }
+        return null;
+    }
+
+    public void remove(int key) {
+        Integer inicialPosition = key % hashmapSize;
+
+        if (hashmap.get(inicialPosition) == key) {
+            hashmap.set(inicialPosition, null);
+            organize(key, inicialPosition);
+        } else {
+
+            Integer position = nextPosition(inicialPosition, increment);
+            if (position == null) {
+                System.out.println("\n404 NOT FOUND");
+            } else {
+                hashmap.set(position, null);
+                organize(key, position);
+            }
+        }
+    }
+
+    public Integer search(int key) {
+        Integer inicialPosition = key % hashmapSize;
+        if (hashmap.get(inicialPosition) != null && hashmap.get(inicialPosition) == key) {
+            return inicialPosition;
+        } else {
+            Integer position = nextPosition(inicialPosition, increment);
+            return position;
+        }
+    }
+
+    public void organize(int lastKey, int index) {
+        int count = 1;
+        int direction = 2;
+        int lastEmpty = index;
+        while (true) {
+            int comparator = index + increment * count * (int) Math.pow(-1, direction);
+            if (hashmap.get(comparator) == null) {
+                break;
+            }
+
+            int hash = hashmap.get(comparator) % hashmapSize;
+
+            if (hash - comparator != 0) {
+                int distance = hash - comparator;
+                int lastDistance = lastEmpty - comparator;
+                int distanceDifference = Math.abs(distance) - Math.abs(lastDistance);
+                if (distanceDifference > 0 || (distanceDifference == 0 && lastDistance < 1)) {
+                    hashmap.set(lastEmpty, hashmap.get(comparator));
+                    hashmap.set(comparator, null);
+                    lastEmpty = comparator;
+                }
+            }
+            direction++;
+
+            comparator = index + increment * count * (int) Math.pow(-1, direction);
+            if (hashmap.get(comparator) == null) {
+                break;
+            }
+
+            hash = hashmap.get(comparator) % hashmapSize;
+
+            if (hash - comparator != 0) {
+                int distance = hash - comparator;
+                int lastDistance = lastEmpty - comparator;
+                int distanceDifference = Math.abs(distance) - Math.abs(lastDistance);
+                if (distanceDifference > 0 || (distanceDifference == 0 && lastDistance < 1)) {
+                    hashmap.set(lastEmpty, hashmap.get(comparator));
+                    hashmap.set(comparator, null);
+                    lastEmpty = comparator;
                 }
             }
             direction++;
             count++;
+            
+            if(count > hashmapSize)break;
         }
     }
 
--- TrabalhoHash/src/Main.java
+++ TrabalhoHash/src/Main.java
@@ -1,15 +1,16 @@
 package src;
+
 import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Main {
 
-    
     public static void main(String[] args) {
         int hashmapSize;
         int mod;
         int increment;
         int key;
+        int cont = 1;
         int menu = 0;
 
         Scanner read = new Scanner(System.in);
@@ -17,7 +18,7 @@ public class Main {
         hashmapSize = read.nextInt();
         System.out.printf("Write the increment ");
         increment = read.nextInt();
-        HashFunctions hash = new HashFunctions(hashmapSize,increment);
+        HashFunctions hash = new HashFunctions(hashmapSize, increment);
 
         hash.tableAutoFill();
 
@@ -33,37 +34,38 @@ public class Main {
             if (menu == 2) {
                 System.out.printf("\n Which key should be inserted? ");
                 key = read.nextInt();
-                hash.insert(key);
+                if (hashmapSize >= cont) {
+                    hash.insert(key);
+                    cont++;
+                } else {
+                    System.out.println("\n Table is full");
+                }
             }
-//            if (menu == 3) {
-//                    System.out.printf("\n Which key should be searched? ");
-//                key = read.nextInt();
-//                if(hash.searchPosition(key,increment) == 0){
-//                    System.out.println("\n The key can't be found!");
-//                }else{
-//                    System.out.println("\n The key position is:  "+ hash.searchPosition(key,increment) +".");
-//                }
-//            }
+            if (menu == 3) {
+                System.out.printf("\n Which key should be searched? ");
+                key = read.nextInt();                
+                Integer index = hash.search(key);
+                if(index == null){
+                    System.out.println("Dont Exist");
+                } else{
+                    System.out.println("\n"+index);
+                }
+            }
             if (menu == 4) {
                 System.out.printf("\n Choose a key to be removed: ");
                 key = read.nextInt();
-                /*if(hash.buscaChave(key) == null){
-                    System.out.println("\nChave nao encontrada!");
-                }else{
-                    hash.removeChaveReinsercao(key);
-                    System.out.println("\nChave removida ");
-                }*/
-            }
-            if (menu == 5) {
-                ArrayList<Integer> map = hash.getHashmap();
-                for (int i = 0; i < map.size(); i++) {
-                    System.out.println(i + "-->" + map.get(i));
-                }
-
+                hash.remove(key);
+                
+        }
+        if (menu == 5) {
+            ArrayList<Integer> map = hash.getHashmap();
+            for (int i = 0; i < map.size(); i++) {
+                System.out.println(i + "-->" + map.get(i));
             }
 
         }
 
     }
- }
-    
+
+}
+}
